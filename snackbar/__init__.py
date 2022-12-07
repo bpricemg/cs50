aedff87da884739cf755ae45164bb59766b2b14f
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-12-07 10:54:50 -0500

    Update __init__.py

--- snackbar/__init__.py
+++ snackbar/__init__.py
@@ -11,13 +11,3 @@ def compiles():
     """snackbar.c compiles"""
     # Check if student code compiles
     check50.c.compile("snackbar.c", lcs50=True)
-
-"""@check50.check(compiles)
-def test1():
-    """burger, fries, soda"""
-    check50.run("./snackbar").stdin("burger").stdin("fries").stdin("soda").stdin("").stdout("\nYour total cost is: $16.50\n")
-
-@check50.check(compiles)
-def test2():
-    """cold brew, hot dog"""
-    check50.run("./snackbar").stdin("cold brew").stdin("hot dog").stdin("").stdout("\nYour total cost is: $8.00\n")"""
