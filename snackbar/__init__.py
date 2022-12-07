02923c37dbe89d22441c9d2f7ac8912051f991fc
bpricemg <43038187+bpricemg@users.noreply.github.com>
2022-12-07 08:22:00 -0500

    Update __init__.py

--- snackbar/__init__.py
+++ snackbar/__init__.py
@@ -1,32 +1,24 @@
 import check50
 import check50.c
+import re
 
 @check50.check()
 def exists():
-    """initials.c exists."""
-    check50.exists("initials.c")
+    """snackbar.c exists"""
+    check50.exists("snackbar.c")
 
 @check50.check(exists)
 def compiles():
-    """initials.c compiles."""
-    check50.c.compile("initials.c", lcs50=True)
+    """snackbar.c compiles"""
+    # Check if student code compiles
+    check50.c.compile("snackbar.c", lcs50=True)
 
 @check50.check(compiles)
-def uppercase():
-    """Outputs BAP for Brandon Alexander Price"""
-    check50.run("./initials").stdin("Brandon Alexander Price", prompt=False).stdout("BAP").exit(0)
+def trinagle1():
+    """burger, fries, soda"""
+    check50.run("./snackbar").stdin("burger").stdin("fries").stdin("soda").stdin("\n").stdout("Your total cost is: $16.50").exit()
 
 @check50.check(compiles)
-def lowercase():
-    """Outputs BAP for brandon alexander price"""
-    check50.run("./initials").stdin("brandon alexander price", prompt=False).stdout("BAP").exit(0)
-
-@check50.check(compiles)
-def mixed_case():
-    """Outputs BP for brandon Price"""
-    check50.run("./initials").stdin("brandon Price", prompt=False).stdout("BP").exit(0)
-
-@check50.check(compiles)
-def all_uppercase():
-    """Outputs B for BRANDON"""
-    check50.run("./initials").stdin("BRANDON", prompt=False).stdout("B").exit(0)
+def trinagle2():
+    """cold brew, hot dog"""
+    check50.run("./snackbar").stdin("cold brew").stdin("hot dog").stdin("\n").stdout("Your total cost is: $8.00").exit()
