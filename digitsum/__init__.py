96222d18b50f31d5e4c088f02bd98dde715e9f27
bpricemg <43038187+bpricemg@users.noreply.github.com>
2025-12-15 08:37:23 -0500

    Add checks for digitsum functionality

---
+++ digitsum/__init__.py
@@ -0,0 +1,29 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    """digitsum.c exists"""
+    check50.exists("digitsum.c")
+
+@check50.check(exists)
+def compiles():
+    """digitsum.c compiles"""
+    # Check if student code compiles
+    check50.c.compile("digitsum.c", lcs50=True)
+
+@check50.check(compiles)
+def positive1():
+    """Digit sum 1234"""
+    check50.run("./digitsum").stdin("1234").stdout("10").exit()
+
+@check50.check(compiles)
+def positive2():
+    """Digit sum 1"""
+    check50.run("./digitsum").stdin("1").stdout("1").exit()
+    
+@check50.check(compiles)
+def negative():
+    """Digit sum -5391"""
+    check50.run("./digitsum").stdin("-5391").stdout("").exit()
